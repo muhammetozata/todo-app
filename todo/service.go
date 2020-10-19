fd1414dc6ec3a9bb1eb087919bb5c3e16a4d8768
Muhammet Ozata <muhammet.ozata@metglobal.com>
2020-10-20 02:25:41 +0300

    service added

---
+++ todo/service.go
@@ -0,0 +1,29 @@
+package todo
+
+import (
+	"context"
+
+	_model "github.com/muhammetozata/todo-app/models"
+)
+
+// todoService ...
+type todoService struct {
+	TodoRepo _model.TodoService
+}
+
+// NewTodoService is a construct
+func NewTodoService(todoRepo _model.TodoRepository) _model.TodoService {
+	return &todoService{
+		TodoRepo: todoRepo,
+	}
+}
+
+func (ts *todoService) GetByID(ctx context.Context, id int64) (_model.Todo, error) {
+	todo, err := ts.TodoRepo.GetByID(ctx, id)
+
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
+}
